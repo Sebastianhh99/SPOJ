74e0b0621042a5360788eaf6ca43fa2fc4e1df0f
Sebastianhh99 <39110763+Sebastianhh99@users.noreply.github.com>
2019-03-21 19:26:52 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 vector<int> cost;
 vector<int> weight;
 int wPigEmpty,wPigFull;
-
+//int INT_MAX=2147493646;
+//Falta memorizar
 int minCost(int actualW,int actualM){
     int ans=INT_MAX;
-    if(actualW==wPigFull) return actualM
-    else if(actualW>wPigFull) return INT_MAX
+    if(actualW==wPigFull) return actualM;
+    else if(actualW>wPigFull) return INT_MAX;
     else{
        for(int i=0;i<cost.size();i++){
            ans=min(ans,minCost(actualW+weight[i],actualM+cost[i]));
@@ -19,10 +21,12 @@ int minCost(int actualW,int actualM){
 }
 
 int main(){
+    cout<<INT_MAX<<endl;
     int cases,n,p,w;
     cin>>cases;
     for(int casee=0;casee<cases;casee++){
-        coins.clear();
+        cost.clear();
+        weight.clear();
         scanf("%d %d",&wPigEmpty,&wPigFull);
         cin>>n;
         for(int i=0;i<n;i++){
@@ -33,7 +37,13 @@ int main(){
         wPigFull-=wPigEmpty;
         if(wPigFull<0) cout<<"This is impossible."<<endl;
         else{
-            cout<<minCost(0,0)<<endl;
+            int ans=minCost(0,0);
+            if(ans==INT_MAX){
+                cout<<"This is impossible."<<endl;
+            }
+            else{
+                cout<<"The minimum amount of money in the piggy-bank is "<<ans<<"."<<endl;
+            }
         }
     }
     return 0;
